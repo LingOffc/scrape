cab55adbb8a265f3e224bfcfa54faa1c889d8bfa
LingOffc <195460521+LingOffc@users.noreply.github.com>
2025-10-15 09:22:22 +0700

    Create scrape.js

---
+++ scrape.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const AUTHTOKEN = '8875'; //isi aja kalo perlu
+const WAYBILL = '201034345270';
+
+async function fetchWaybill(waybillNo) {
+  const url = 'https://office.jtcargo.co.id/official/waybill/trackingCustomerByWaybillNo';
+
+  try {
+    const resp = await axios.post(url,
+      {
+        waybillNo: waybillNo,
+        searchWaybillOrCustomerOrderId: "1"
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authtoken': AUTHTOKEN,
+          'Accept': 'application/json, text/plain, /',
+          'Origin': 'https://www.jtcargo.id',
+        },
+        timeout: 20000
+      }
+    );
+
+    
+    console.log(resp.data); //mau liat detailnya ubah jadi resp.data.details
+    return resp.data; //bagian ini juga sama kayak yang diatas
+  } catch (err) {
+    console.error('Error:', err.response ? err.response.status : err.message);
+    if (err.response && err.response.data) console.error('Response body:', err.response.data);
+    throw err;
+  }
+}
+
+fetchWaybill(WAYBILL).catch(()=>process.exit(1));
